0061c6b07a4f693873cb6738dcb899b4a41f26e0
10000JI <sksk436@naver.com>
2024-04-02 20:23:28 +0900

    단계별 풀어보기 (브루트포스)

--- src/Baekjoon/step12/ExampleSix.java
+++ src/Baekjoon/step12/ExampleSix.java
@@ -6,6 +6,20 @@ public class ExampleSix {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
+        int answer = 0;
+        while (n > 0) {
+            if (n % 5 == 0) {
+                answer += n / 5;
+                System.out.println(answer);
+                return;
+            }
+            if (n < 3) {
+                System.out.println(-1);
+                return;
+            }
+            n -= 3;
+            answer++;
+        }
+        System.out.println(answer);
     }
 }
