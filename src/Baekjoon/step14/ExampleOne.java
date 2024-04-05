a56ce7ddc7bafd0a0da3f5a422d24ac927135cb1
10000JI <sksk436@naver.com>
2024-04-05 14:17:50 +0900

    단계별 풀어보기 (멥)

---
+++ src/Baekjoon/step14/ExampleOne.java
@@ -0,0 +1,26 @@
+package Baekjoon.step14;
+
+import java.io.*;
+import java.util.*;
+
+public class ExampleOne {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(set.contains(num) ? "1 " : "0 ");
+        }
+
+        System.out.println(sb);
+    }
+}
