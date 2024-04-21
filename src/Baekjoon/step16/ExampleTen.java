e7cd15e0b8f13f5ea2230dd24b78d7f445e33842
10000JI <sksk436@naver.com>
2024-04-22 01:37:35 +0900

    단계별 풀어보기 (스택,큐,덱)

---
+++ src/Baekjoon/step16/ExampleTen.java
@@ -0,0 +1,12 @@
+package Baekjoon.step16;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class ExampleTen {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}
