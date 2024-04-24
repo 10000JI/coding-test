7b31d61b3307be26f4bc8985573ad40a8fda08c8
10000JI <sksk436@naver.com>
2024-04-24 10:08:46 +0900

    구간합 (prefixSum)

---
+++ src/PrefixSum/Example10986.java
@@ -0,0 +1,41 @@
+package PrefixSum;
+
+import java.io.*;
+import java.util.*;
+
+public class Example10986 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 입력 받기
+        int n = Integer.parseInt(st.nextToken()); // 배열의 길이
+        int m = Integer.parseInt(st.nextToken()); // 나눌 수
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n]; // 입력 배열
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // Prefix Sum 배열 생성
+        long sum = 0;
+        long[] count = new long[m]; // 나머지 카운트 배열
+        long result = 0; // 결과값
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            sum %= m; // 나머지 계산
+            count[(int) sum]++; // 나머지 카운트 증가
+        }
+
+        // 나머지가 0인 경우 처리
+        result += count[0];
+
+        // 나머지가 같은 부분 합에서 2개씩 뽑는 조합 계산
+        for (int i = 0; i < m; i++) {
+            result += (count[i] * (count[i] - 1)) / 2;
+        }
+
+        System.out.println(result); // 결과 출력
+    }
+}
---
+++ src/PrefixSum/Example2851.java
@@ -0,0 +1,41 @@
+package PrefixSum;
+
+import java.io.*;
+import java.util.*;
+
+public class Example2851 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[11];
+        int sum = 0;
+
+        //누적합 배열
+        for (int i = 1; i < 11; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 1; i < 11; i++) {
+            //합한 arr이 100인 숫자가 있다면 sum에 100대입
+            if (arr[i] == 100) {
+                sum = 100;
+                break;
+            }
+            //100보다 크다면 100과 가까운 수 sum에 대입, 만약 100에 가까운 수가 2개라면 (예: 98, 102) 마리오는 큰 값을 선택
+            else if (arr[i] > 100) {
+                int a = arr[i] - 100;
+                int b = 100 - arr[i - 1];
+                if (a == b) {
+                    sum = arr[i];
+                    break;
+                } else {
+                    sum = (a > b) ? arr[i - 1] : arr[i];
+                    break;
+                }
+            }
+        }
+        if (sum == 0) {
+            sum = arr[10];
+        }
+        System.out.println(sum);
+    }
+}
