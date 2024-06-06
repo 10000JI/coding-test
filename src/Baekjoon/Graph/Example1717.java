e29f7006f39a4955ec015453517084b409c275f9
10000JI <sksk436@naver.com>
2024-06-06 10:12:19 +0900

    그래프-유니온파인드)1717번 집합의 표현

---
+++ src/Baekjoon/Graph/Example1717.java
@@ -0,0 +1,68 @@
+package Baekjoon.Graph;
+
+import java.io.*;
+import java.util.*;
+
+public class Example1717 {
+
+    static int[] parent;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (num == 0) {
+                union(a, b);
+            } else if (num == 1) {
+                sb.append(isSameParent(a, b) ? "YES" : "NO").append("\n");
+            } else {
+                continue;
+            }
+        }
+        System.out.println(sb);
+    }
+
+    static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            if (x < y) {
+                parent[y] = x;
+            } else {
+                parent[x] = y;
+            }
+        }
+    }
+
+    public static boolean isSameParent(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x == y) {
+            return true;
+        }
+        return false;
+    }
+}
